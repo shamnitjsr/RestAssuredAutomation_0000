2f1f0d34a21d7ef7a6e947a6cca92363b317926c
shamnitjsr <shamnitjsr@gmail.com>
2024-09-29 22:00:06 +0530

    Implementation of Test Class

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -10,9 +10,20 @@ Delete User(Delete)-->	https://petstore.swagger.io/v2/user/{username}
 */
 
 public class Routes {
+
+	public static String base_url = "https://petstore.swagger.io/v2";
+
+	// User Model
+
+	public static String post_url = base_url + "/user";
+	public static String get_url = base_url + "/user/{username}";
+	public static String update_url = base_url + "/user/{username}";
+	public static String delete_url = base_url + "/user/{username}";
 	
-	public static String base_url = "https://petstore.swagger.io";
+	//Store module
+		//Here you will create Store module URL's
+	//Pet module
+		//Here you will create Pet module URL's
 	
-	//User Model
 
 }
