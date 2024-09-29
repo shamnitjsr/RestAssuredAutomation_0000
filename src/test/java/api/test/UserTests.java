95059a456b2c65e3dcd25479d00086f3cbe4f983
shamnitjsr <shamnitjsr@gmail.com>
2024-09-30 02:07:18 +0530

    implementation of ExtentReport

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -43,4 +43,36 @@ public class UserTests {
 		
 	}
 
+	@Test(priority=2)
+	public void testGetUserByName() {
+		Response response = UserEndPoints.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+	}
+	
+	@Test(priority=3)
+	public void testUpdateUserByName() {
+		
+		//update data using payload
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		
+		Response response = UserEndPoints.updateUser(this.userPayload.getUsername(), userPayload);
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		//Checking data after update
+		Response responseAfterUpdate = UserEndPoints.readUser(this.userPayload.getUsername());
+		Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);
+	}
+	
+	@Test(priority=4)
+	public void testDeleteUserByName() {
+		
+		Response response = UserEndPoints.deleteUser(this.userPayload.getUsername());
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+	}
 }
